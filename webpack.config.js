ca0bc4b1f9d069b34d514fd0f7e31f159eebb221
Claudio Maccari <claudio@jitidea.net>
2020-03-21 23:53:54 +0100

    build app with webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: {
+        app: './src/app.js',
+        // background: './src/background.js',
+        // devtools: './src/devtools.js',
+        // content: './src/content.js',
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false
+        }),
+        new CopyWebpackPlugin([
+            { from: path.resolve(__dirname, "src/background.js") },
+            { from: path.resolve(__dirname, "src/content.js") },
+            { from: path.resolve(__dirname, "src/devtools.html") },
+            { from: path.resolve(__dirname, "src/devtools.js") },
+            { from: path.resolve(__dirname, "src/manifest.json") },
+            { from: path.resolve(__dirname, "src/panel.html") },
+        ])
+    ],
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
